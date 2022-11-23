f508ba01c5afbd3154b2ab27cd9523043e5e81fb
sudilamunasinghe <sudilamunasinghe@gmail.com>
2022-11-23 09:49:24 +0530

    Created JQuery Part for Responsive Navigation Bar

--- aboutus.js
+++ aboutus.js
@@ -1,3 +1,64 @@
+/*--JS Part for Responsive Navigation Bar-- */
+
+$('#bar').click(function(){
+	$('.side-content').css({
+		display : 'block',
+	})
+	$('.sidenav').css({
+		transform : 'translateX(0%)',
+		transition: '0.5s'
+	})
+	$('.navbar').css({
+		display : 'none'
+	})
+	$('#close').css({
+		display: 'block'
+	})
+	$('.blur').css({
+		transition: '0.5s',
+		// filter: 'blur(40px)'
+		opacity: '0.3'
+	})
+	$('body').css({
+		// overflowY: 'hidden'
+	})
+})
+$('#close').click(function(){
+	
+	$('.sidenav').css({
+		transform : 'translateX(-100%)',
+		transition: '0.5s'
+	})
+	$('.side-content').css({
+		display: 'none'
+	})
+	$('.navbar').css({
+		display: 'block',
+		// position: 'absolute',
+		// zIndex: '-1'
+	})
+	$('#close').css({
+		display: 'none'
+	})
+	$('#bar').css({
+		display: 'block'
+	})
+	$('.slider').css({
+		filter: 'blur(0px)',
+		opacity: '1'
+	})
+	$('body').css({
+		overflowY: 'scroll'
+	})
+	$('.blur').css({
+		transition: '0.5s',
+		filter: 'blur(0px)',
+		opacity: '1'
+	})
+})
+
+/*--JS Part for why-choose section---*/
+
 $('.why-choose-about-more').click(function(){
     var x = $(this).text();
     if (x == "View more"){
